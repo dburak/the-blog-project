0c51e4a19890534f456db7803c448439fbafb857
burakdiker <burak.diker@ug.bilkent.edu.tr>
2022-11-14 16:20:48 +0300

    blog-component-rearranged-with-props

--- frontend-react/src/components/Home/Home.js
+++ frontend-react/src/components/Home/Home.js
@@ -1,9 +1,41 @@
-import React from 'react'
+
+import React, {useState, useEffect} from "react"
+import Blog from "../Blog/Blog"
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [blogList, setBlogList] = useState([])
+
+  useEffect(() => {
+    fetch("/blogs")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true)
+        setBlogList(result)
+      },
+      (error) => {
+        setIsLoaded(true)
+        setError(error)
+      }
+    )
+  }, [])
+
+  if(error) {
+    return <div>Error</div>
+  } else if(!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+     <div>
+      Home
+      {blogList.map(blog => (
+        <Blog title={blog.blogTitle} content={blog.blogContent}></Blog>
+      ))}
+     </div>
+    )
+  }
 }
 
 export default Home
